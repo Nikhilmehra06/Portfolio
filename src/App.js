88b7eb896f79577b3216b543fa1ab8b4c8641c04
Nikhil <Nikhilmehra7919@gmail.com>
2022-04-06 01:28:55 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import About from './components/About';
 import Card from './components/Card';
 import Footer from './components/Footer';
 import Skill from './components/Skill';
 import Contact from './components/Contact';
+import aos from 'aos';
+import 'aos/dist/aos.css';
 
 export default function App() {
+  useEffect(() => {
+    aos.init({
+      once: true,
+    });
+  });
+
   return (
     <div className="min-h-screen bg-black py-10 px-3 sm: px-5 bg-gray-100">
-      <Card />
-      <About />
+      <div data-aos="fade-down" data-aos-duration="800">
+        <Card />
+      </div>
+      <div data-aos="fade-up" data-aos-duration="800" data-aos-delay>
+        <About />
+      </div>
+
       <Skill />
       <Contact />
       <Footer />
